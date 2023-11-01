266f0f1120491eaf9db2315ae48d0a563b337c71
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-01 21:06:33 +0100

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  if (number1 < 0 || number2 < 0) return "ERROR";
+  if (!Number.isInteger(number1) || !Number.isInteger(number2)) return "ERROR";
 
+  let sum = 0;
+  let lowerLimit = number1 < number2 ? number1 : number2;
+  let upperLimit = number1 < number2 ? number2 : number1;
+
+  for (let i = lowerLimit; i <= upperLimit; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
