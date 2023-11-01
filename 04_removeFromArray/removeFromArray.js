b518494fae53d4369e0c7c1e95f96778c91140d2
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-01 20:46:28 +0100

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  for (value of values) {
+    for (let i = 0; i < array.length; i++) {
+      if (value === array[i]) array.splice(i, 1);
+    }
+  }
 
+  return array;
 };
 
 // Do not edit below this line
