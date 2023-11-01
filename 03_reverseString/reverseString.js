22f64fdf9d431dda0eea64576a6531e181066b1d
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-01 20:37:25 +0100

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (text) {
+  let char = text.split("");
+  let result = "";
 
+  for (let i = char.length - 1; i >= 0; i--) {
+    result += char[i];
+  }
+
+  return result;
 };
 
 // Do not edit below this line
