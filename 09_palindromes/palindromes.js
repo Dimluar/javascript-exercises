d45e7c56e7846db36e6459ae08deec83ceec7c89
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-04 18:27:11 +0100

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let newStr = str.replace(/[^\p{L}\p{N}]/gu, "").toLowerCase();
+  return newStr === newStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
