9532feb0bdb4f5cdd29fcb628337bed2ae9310fb
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-04 19:59:57 +0100

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let arrayCopy = array.slice();
+  arrayCopy.sort((a, b) => {
+    let aAge = a.hasOwnProperty("yearOfDeath")
+      ? a.yearOfDeath - a.yearOfBirth
+      : new Date().getFullYear() - a.yearOfBirth;
+    let bAge = b.hasOwnProperty("yearOfDeath")
+      ? b.yearOfDeath - b.yearOfBirth
+      : new Date().getFullYear() - b.yearOfBirth;
 
+    return bAge - aAge;
+  });
+  return arrayCopy[0];
 };
 
 // Do not edit below this line
