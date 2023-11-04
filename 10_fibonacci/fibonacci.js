3888752a353b7ea3413e7f2ed3140c1628e07dc7
Dimluar <148585902+Dimluar@users.noreply.github.com>
2023-11-04 19:43:25 +0100

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (place) {
+  place = +place;
 
+  if (place < 0) return "OOPS";
+
+  switch (place) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fibonacci(place - 1) + fibonacci(place - 2);
+      break;
+  }
 };
 
 // Do not edit below this line
